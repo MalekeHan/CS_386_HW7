8ed33387e753705061b50b6bd3b2c866f38b43bb
MalekeHan <Malekehanson@gmail.com>
2024-02-27 16:24:10 -0800

    Just need to do intravels correctly

--- pool_attack.go
+++ pool_attack.go
@@ -13,112 +13,48 @@ import (
 // - Compute the same key pair that the victim computes
 // - Use this key pair to sign `document` and return the signature
 func runAttacker(o *os, document string, victimSleepsBeforeGenerateKey time.Duration) []byte {
-	// Your attack will probably want to make use of the following
-	// functions/methods/values:
-	// - time.Now
-	// - time.Sleep
-	// - (*os).draw
-	// - poolInitState
-	// - poolAdd
-	// - prngInit
-	// - prngDraw
-	// - generateRsaKeyPair
-	// - (*rsaPrivateKey).sign
-	//
-	// Finally, we recommend that you don't try to call `(*os).draw` at exactly
-	// the same time as a sample is added or as the victim performs their own
-	// draw. If you do, you won't be sure whether your draw came before or after
-	// the other event. Instead, we recommend leaving a good amount of time
-	// between other events. For example, if you know that the OS will add a
-	// sample at t = 8 and that the victim will draw at t = 8.5, and you are
-	// trying to draw your own sample in between these events, we recommend
-	// drawing your sample at t = 8.25.
+	startTime := time.Now()
+	initialSleep := 250 * time.Millisecond
+	interval := 1 * time.Second
 
-	/*
-		t = 1.5
-		s0 occurs at 0 seconds
-		1s we get our
-		1.5 seconds the victim draws
-			- we would need to call draw at 1.25 seconds for the "A"
-			- V happens at 1.5
-
-		- so i am going through {0,1}^16 looking for one that matches for what I get at 1.25?
-
-		Do the poolinit to get the original state
-		- do go through all 2^16 and and run the add and then the draw
-		- sample at 1.25
-		- find the one that matches
-		- do draw on it one more time
-	*/
-
-	interval := 1250 * time.Millisecond // Interval of 1.25 seconds
-	startTime := time.Now()             // Capture the start time
-	firstInterval := true
+	time.Sleep(initialSleep)
+	var correctDraw prngSample
+	var correctSign []byte
 
 	// go run pool.go pool_solution.go 1.5s
 
+	s0 := poolInitState // get the initialstate of the entropy pool -- s0
+
 	for {
+		// Sleep for the interval
+		time.Sleep(interval)
 		// Check if the total duration has been reached
 		if time.Since(startTime) >= victimSleepsBeforeGenerateKey {
 			break // Exit the loop if the specified duration is reached
 		}
 
-		if firstInterval {
-			s0 := poolInitState
-			firstInterval = false
+		os_draw := o.draw()
 
-			for i := 0; i < 1<<16; i++ {
-				var sample poolSample
-				sample[0] = byte(i >> 8)   // High byte
-				sample[1] = byte(i & 0xFF) // Low byte
+		for i := 0; i < 1<<16; i++ {
+			var canidate poolSample
+			canidate[0] = byte(i >> 8)   // High byte
+			canidate[1] = byte(i & 0xFF) // Low byte
 
-				s1 := poolAdd(s0, sample)
-				i_0 := prngInit(prngSeed(s1)) // this does the init part
-				i_1, a := prngDraw(i_0)
-
-				//guessedState, _ := prngDraw(initState) // this is the draw that we got from the sample // but how do i check if this matched? should i just save this? and then get to 1.25 and see if i have it?
-
-				//seedPrng := newPrng(prngSeed(guessedState))
-
-				//_, private := generateRsaKeyPair(seedPrng)
-
-				//signedDoc := private.sign([]byte(document))
-
-				return nil
+			s1 := poolAdd(s0, canidate)                              // do the add
+			currentState := prngInit(prngSeed(s1))                   // do the init
+			state_from_draw, guessedSample := prngDraw(currentState) // do the draw
+			if os_draw == guessedSample {                            // check if the pnrg draw matches the os_draw
+				debugPrintf("I GOT THE CORRECT SAMPLE: [%x ]\n", guessedSample)
+				_, correctDraw = prngDraw(state_from_draw)
+				prngGuess := newPrng(prngSeed(correctDraw))
+				_, priv_key := generateRsaKeyPair(&prngGuess) // generateRsaKeyPair
+				documentBytes := []byte(document)             // Convert document string to []byte
+				correctSign = priv_key.sign(documentBytes)    // Use the sign method to sign the document
 			}
 
 		}
 
-		// Perform the attack operations here
-
-		// Example: Attempt to guess the PRNG state or observe the side effects of the key generation
-		// This could involve drawing from the PRNG, attempting to generate a key pair, or other operations
-		// that would exploit the predictable state of the PRNG or timing information.
-
-		// Sleep for the interval
-		time.Sleep(interval)
 	}
 
-	s0 := poolInitState // get the initialstate of the entropy pool -- s0
-
-	// enumerate through all the r's
-	for i := 0; i < 1<<16; i++ {
-		var sample poolSample
-		sample[0] = byte(i >> 8)   // High byte
-		sample[1] = byte(i & 0xFF) // Low byte
-
-		//newState := poolAdd(s0, sample)
-		//initState := prngInit(prngSeed(newState)) // this does the init part
-
-		//guessedState, _ := prngDraw(initState) // this is the draw that we got from the sample // but how do i check if this matched? should i just save this? and then get to 1.25 and see if i have it?
-
-		//seedPrng := newPrng(prngSeed(guessedState))
-
-		//_, private := generateRsaKeyPair(seedPrng)
-
-		//signedDoc := private.sign([]byte(document))
-
-		return nil
-	}
-	return nil
+	return correctSign
 }
