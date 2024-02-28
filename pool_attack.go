67fc7073ab117a44f396af336bd888cd07d7a2ff
MalekeHan <Malekehanson@gmail.com>
2024-02-28 15:53:39 -0800

    Finished

--- pool_attack.go
+++ pool_attack.go
@@ -4,62 +4,65 @@ import (
 	"time"
 )
 
-// Run the attacker process.
-//
-// The attacker is told how long the victim will sleep before drawing from the
-// OS in order to generate its key pair.
-//
-// The attacker's job is to:
-// - Compute the same key pair that the victim computes
-// - Use this key pair to sign `document` and return the signature
 func runAttacker(o *os, document string, victimSleepsBeforeGenerateKey time.Duration) []byte {
 	startTime := time.Now()
 	initialSleep := 250 * time.Millisecond
 	interval := 1 * time.Second
 
+	// Initial sleep before starting the attack process
 	time.Sleep(initialSleep)
 	var correctDraw prngSample
 	var correctSign []byte
+	currentPoolState := poolInitState // Start with the initial state of the entropy pool
 
-	// go run pool.go pool_solution.go 1.5s
-
-	s0 := poolInitState // get the initialstate of the entropy pool -- s0
+	hasSigned := false // Flag to check if signing has occurred
 
 	for {
-		// Sleep for the interval
-		time.Sleep(interval)
+
+		time.Sleep(interval) // Sleep for the interval duration
+
 		// Check if the total duration has been reached
 		if time.Since(startTime) >= victimSleepsBeforeGenerateKey {
-			break // Exit the loop if the specified duration is reached
+			break // Exit the loop after signing
 		}
 
-		os_draw := o.draw() // draw once per intraval
+		os_draw := o.draw() // Draw once per interval from the OS
 
 		for i := 0; i < 1<<16; i++ {
-			var canidate poolSample
-			canidate[0] = byte(i >> 8)   // High byte
-			canidate[1] = byte(i & 0xFF) // Low byte
+			var candidate poolSample
+			candidate[0] = byte(i >> 8)   // High byte
+			candidate[1] = byte(i & 0xFF) // Low byte
 
-			s1 := poolAdd(s0, canidate)                              // do the add
-			currentState := prngInit(prngSeed(s1))                   // do the init
-			state_from_draw, guessedSample := prngDraw(currentState) // do the draw for A -- this also gives the PRNGState of A
-			if os_draw == guessedSample {                            // check if the pnrg draw matches the os_draw
+			// Update the pool state with the candidate for this interval
+			nextPoolState := poolAdd(currentPoolState, candidate)
+			prng := prngInit(prngSeed(nextPoolState))        // Initialize PRNG with the updated pool state
+			state_from_draw, guessedSample := prngDraw(prng) // Perform a PRNG draw with the updated state
 
-				// here we need to store the state of the prng and use it for the next intreval
+			// draw needs to change the second one
 
-				// we only sign the document after we have gone through all the intrevals
+			//print these things out and see
 
+			if os_draw == guessedSample {
 				debugPrintf("I GOT THE CORRECT SAMPLE: [%x ]\n", guessedSample)
-				_, correctDraw = prngDraw(state_from_draw) // this does the second draw to get to V from A -- this also gives the PRNGState of V
-				prngGuess := newPrng(prngSeed(correctDraw))
-				_, priv_key := generateRsaKeyPair(&prngGuess) // generateRsaKeyPair
-				documentBytes := []byte(document)             // Convert document string to []byte
-				correctSign = priv_key.sign(documentBytes)    // Use the sign method to sign the document
+				// If the PRNG draw matches the OS draw, prepare for the next interval
+				_, correctDraw = prngDraw(state_from_draw) // This does the second draw to get to V from A
+				currentPoolState = nextPoolState           // Update the pool state for the next interval
+				break                                      // Found the correct sample, break to update for the next interval
 			}
-
 		}
 
 	}
 
-	return correctSign
+	debugPrintf("GOING TO SIGN NOW\n")
+
+	if !hasSigned {
+		// Perform the signing in the last interval
+		prngGuess := newPrng(prngSeed(correctDraw))
+		_, privKey := generateRsaKeyPair(&prngGuess) // Generate RSA key pair
+		documentBytes := []byte(document)            // Convert document string to []byte
+		correctSign = privKey.sign(documentBytes)    // Sign the document
+		hasSigned = true
+	}
+
+	return correctSign // Return the signature
 }
