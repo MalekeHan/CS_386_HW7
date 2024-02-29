eafe7dd3a9eaf5ac9476f3c765c71d216c4724a0
MalekeHan <Malekehanson@gmail.com>
2024-02-28 16:31:51 -0800

    need to fix for 16+ secs

--- pool_attack.go
+++ pool_attack.go
@@ -14,6 +14,7 @@ func runAttacker(o *os, document string, victimSleepsBeforeGenerateKey time.Dura
 	var correctDraw prngSample
 	var correctSign []byte
 	currentPoolState := poolInitState // Start with the initial state of the entropy pool
+	var maximumDraws int
 
 	hasSigned := false // Flag to check if signing has occurred
 
@@ -25,29 +26,31 @@ func runAttacker(o *os, document string, victimSleepsBeforeGenerateKey time.Dura
 		if time.Since(startTime) >= victimSleepsBeforeGenerateKey {
 			break // Exit the loop after signing
 		}
+		maximumDraws += 1
+		if maximumDraws < 16 {
+			os_draw := o.draw() // Draw once per interval from the OS
 
-		os_draw := o.draw() // Draw once per interval from the OS
+			for i := 0; i < 1<<16; i++ {
+				var candidate poolSample
+				candidate[0] = byte(i >> 8)   // High byte
+				candidate[1] = byte(i & 0xFF) // Low byte
 
-		for i := 0; i < 1<<16; i++ {
-			var candidate poolSample
-			candidate[0] = byte(i >> 8)   // High byte
-			candidate[1] = byte(i & 0xFF) // Low byte
+				// Update the pool state with the candidate for this interval
+				nextPoolState := poolAdd(currentPoolState, candidate)
+				prng := prngInit(prngSeed(nextPoolState))        // Initialize PRNG with the updated pool state
+				state_from_draw, guessedSample := prngDraw(prng) // Perform a PRNG draw with the updated state
 
-			// Update the pool state with the candidate for this interval
-			nextPoolState := poolAdd(currentPoolState, candidate)
-			prng := prngInit(prngSeed(nextPoolState))        // Initialize PRNG with the updated pool state
-			state_from_draw, guessedSample := prngDraw(prng) // Perform a PRNG draw with the updated state
+				// draw needs to change the second one
 
-			// draw needs to change the second one
+				//print these things out and see
 
-			//print these things out and see
-
-			if os_draw == guessedSample {
-				debugPrintf("I GOT THE CORRECT SAMPLE: [%x ]\n", guessedSample)
-				// If the PRNG draw matches the OS draw, prepare for the next interval
-				_, correctDraw = prngDraw(state_from_draw) // This does the second draw to get to V from A
-				currentPoolState = nextPoolState           // Update the pool state for the next interval
-				break                                      // Found the correct sample, break to update for the next interval
+				if os_draw == guessedSample {
+					debugPrintf("I GOT THE CORRECT SAMPLE: [%x ]\n", guessedSample)
+					// If the PRNG draw matches the OS draw, prepare for the next interval
+					_, correctDraw = prngDraw(state_from_draw) // This does the second draw to get to V from A
+					currentPoolState = nextPoolState           // Update the pool state for the next interval
+					break                                      // Found the correct sample, break to update for the next interval
+				}
 			}
 		}
 
